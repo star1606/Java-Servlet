54d89285cb348010ff5fe9f35d0ebbc9210800fa
Lee <swiniee@naver.com>
2020-11-10 23:16:20 +0900

    DispatcherServlet으로 애플리케이션 컨텍스트 만들기, MVC연동

--- src/main/java/me/lee/MyFilter.java
+++ src/main/java/me/lee/MyFilter.java
@@ -2,6 +2,7 @@ package me.lee;
 
 import java.io.IOException;
 
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
--- src/main/java/me/lee/WebConfig.java
+++ src/main/java/me/lee/WebConfig.java
@@ -4,9 +4,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
+
+// 다른건 스캔 안하고 컨트롤러만 스캔하겠다
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-// 컨트롤러는 빈으로 등록하지 않겠다.
-public class AppConfig {
+@ComponentScan(useDefaultFilters = false, includeFilters = @ComponentScan.Filter(Controller.class) )
+public class WebConfig {
 
 }
