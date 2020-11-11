8c5a822250c421b76ee6255336ba4d3aa2df2c27
Lee <swiniee@naver.com>
2020-11-11 23:10:22 +0900

    bean으로 일일이 핸들러를 설정하는 고전적인 방법

--- src/main/java/me/lee/WebConfig.java
+++ src/main/java/me/lee/WebConfig.java
@@ -3,8 +3,14 @@ package me.lee;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
@@ -14,6 +20,22 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan
 public class WebConfig {
 
+	@Bean
+	public HandlerMapping handleMapping() {
+		RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+		handlerMapping.setInterceptors();
+		handlerMapping.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return handlerMapping;
+	}
+	
+	@Bean
+	public HandlerAdapter handlerAdapter() {
+		RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+		// argument를 만들어서 보낼 수 있다.
+		//handlerAdapter.seta
+		return handlerAdapter;
+	}
+	
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
