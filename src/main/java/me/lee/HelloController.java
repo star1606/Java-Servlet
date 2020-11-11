14688b4451b5491d57d00a0ece164ebee276736c
Lee <swiniee@naver.com>
2020-11-11 15:29:20 +0900

    DispatcherServlet

--- src/main/java/me/lee/HelloController.java
+++ src/main/java/me/lee/HelloController.java
@@ -2,6 +2,7 @@ package me.lee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,14 @@ public class HelloController {
 	
 	
 	@GetMapping("/hello")
+	@ResponseBody
 	public String hello() {
 		return "Hello, DispatcherServlet used," + helloService.getName();
 	}
-	
+	// 뷰 객체 찾아지고 뷰에다가 모델 객체를 바인딩해서 뷰를 렌더링함
+	// 뷰를 리스폰스에다가 실어서 보냄
+	@GetMapping("/sample")
+	public String sample() {
+		return "/WEB-INF/sample.jsp";
+	}
 }
